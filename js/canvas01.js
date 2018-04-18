c117be6d16f2e6408513fb0c0bb1afbafd9d5d40
karrollinaaa <karolinab503@gmail.com>
2018-04-18 07:45:30 +0200

    pablo

---
+++ js/canvas01.js
@@ -0,0 +1,7 @@
+/*jshint esversion: 6 */
+const canvasElem = document.getElementById('canvas');
+const ctx = canvasElem.getContext('2d');
+
+//rysowanie prostokąta
+//x,y, width, height
+ctx.fillRect(25, 25, 100, 100);
